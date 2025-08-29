143d97f990d373ca1656861b5fc9200d2a8b0019
Michael Schaferkotter <michael.r.schaferkotter.ctr@us.navy.mil>
2025-08-29 15:51:17 -0500

    Add versioneer generated _version.py

--- oceanmesh/__init__.py
+++ oceanmesh/__init__.py
@@ -129,3 +129,6 @@ __version__ = _version.get_versions()["version"]
 from . import _version
 
 __version__ = _version.get_versions()["version"]
+
+from . import _version
+__version__ = _version.get_versions()['version']
